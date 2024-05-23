bb263b108ac4d2426f3fd8b4ae9396672e121b3e
lawrence307 <lawrence_muthini@yahoo.com>
2024-05-23 11:58:46 +0300

    data preparation

---
+++ data.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import os
+import yfinance as yf
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Download historical data for a specific stock
+ticker = 'SCOM'
+data = yf.download(ticker, start='2020-01-01', end='2024-01-01')
+print(data.head())
+data.to_csv('SCOM_HistoricalPrices.csv')
+df = pd.read_csv('SCOM_HistoricalPrices.csv')
+
+# Display the first few rows of the dataset
+print(df.tail())
+#check the datatypes and missing values
+print(df.info())
+print(df.isnull().sum())
+print(df.columns)
+df.columns = df.columns.str.strip()
+print("Columns after stripping:", df.columns)
+#summary
+print(df.describe())
+#check for duplicates
+print(df.duplicated().sum())
+#display hist
+df.hist(figsize=(10, 10))
+plt.show()
+
+
+# Standardize date format and string columns
+df['date'] = pd.to_datetime(df['date'])
+df['category'] = df['category'].str.lower()
+
+# Handle missing data
+df = df.dropna()  # or use df['column_name'] = df['column_name'].fillna(df['column_name'].mean())
+
+# Validate data
+df = df[df['price'] > 0]  # Ensure prices are positive
+
+print(df.head())
+print(df.info())
+
+
+
